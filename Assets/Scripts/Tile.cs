1f828e4adcfa0792f82c76be623d07321ca61b28
BrendanMulcahy <bmm58@case.edu>
2015-05-02 13:12:47 -0400

    Created the scene & Tiles

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+    private bool _isDirty = false;
+
+    public bool IsDirty
+    {
+        get
+        {
+            return _isDirty;
+        }
+        set
+        {
+            _isDirty = value;
+            GetComponent<Renderer>().material.color = _isDirty ? Color.red : Color.white;
+        }
+    }
+}
